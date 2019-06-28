00117ac6239c51714b3165e853267d06d2235f8c
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-06-28 09:00:45 -0300

    CRUD

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,10 @@ const ProductController = require('./controllers/ProductControllers');
 
 //Primeira Rota
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
 
-routes.post('/products', ProductController.store)
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.updade);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
