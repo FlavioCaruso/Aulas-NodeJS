d4e5e4bef4212b65a51cc99043eccf1653442dd6
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-06-28 09:13:19 -0300

    Curso NodeJS finalizado

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 // npm run dev
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', { useNewUrlParser: true });
